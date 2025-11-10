15e73dabf728aa34c07480c6626c3a2d41058647
Daniel <daniel@web.de>
2025-11-10 15:10:00 +0100

    Database Connection :3

--- src/main/java/org/fpj/App.java
+++ src/main/java/org/fpj/App.java
@@ -1,11 +1,16 @@
 package org.fpj;
 
 import javafx.application.Application;
+import org.fpj.database.Connection;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connect = new Connection();
+        Connection conn = connect.getConnection();
         Application.launch(JavafxApp.class, args);
     }
 }
