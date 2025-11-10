7dbd2d4eeddbfc649a931419cf922e24f7b1c02e
colin <colingkirchner@gmail.com>
2025-11-10 19:13:33 +0100

    WIP: Connection in main entfernt

--- src/main/java/org/fpj/App.java
+++ src/main/java/org/fpj/App.java
@@ -1,16 +1,11 @@
 package org.fpj;
 
 import javafx.application.Application;
-import org.fpj.database.Connection;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) throws SQLException {
-        Connection connect = new Connection();
-        Connection conn = connect.getConnection();
+    public static void main(String[] args){
         Application.launch(JavafxApp.class, args);
     }
 }
