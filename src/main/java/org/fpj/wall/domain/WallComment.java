c2a3cc839fd2806d08d05f97d44a9d687235122c
colin <colingkirchner@gmail.com>
2025-11-10 15:10:45 +0100

    WIP: WallComment

--- src/main/java/org/fpj/wall/domain/WallComment.java
+++ src/main/java/org/fpj/wall/domain/WallComment.java
@@ -1,6 +1,27 @@
 package org.fpj.wall.domain;
 
-/** Auto-generated skeleton. */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class WallComment {
-    // TODO: implement
+
+    private Long id;
+    private String content;
+    private Long wallOwnerId;
+    private Long authorId;
+    private Timestamp createdAt;
+
+    public WallComment(final String content, final Long wallOwnerId,
+                       final Long authorId, final Timestamp createdAt){
+        this(null, content, wallOwnerId, authorId, createdAt);
+    }
+
+
+
 }
--- src/main/java/org/fpj/wall/domain/WallCommentRepository.java
+++ src/main/java/org/fpj/wall/domain/WallCommentRepository.java
@@ -1,6 +1,24 @@
 package org.fpj.wall.domain;
 
-/** Auto-generated skeleton. */
-public interface WallCommentRepository {
-    // TODO: implement
+import org.fpj.users.domain.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class WallCommentRepository {
+
+    //private Connection conn;
+
+//    @Autowired
+//    public WallCommentRepository(Connection conn){
+//        this.conn = conn;
+//    }
+
+
+//    public List<WallComment> getWallCommentsByUser(User wallOwner){
+//
+//    }
 }
+
+
