541b078aa8578f791becca032ed40d0315627dba
colin <colingkirchner@gmail.com>
2025-11-15 16:03:03 +0100

    WIP: LoginTest + regexValidator

--- src/main/java/org/fpj/users/application/LoginService.java
+++ src/main/java/org/fpj/users/application/LoginService.java
@@ -9,10 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class LoginService {
+
+    private static final String REGEX_USERNAME_VALIDATOR =
+            "^(?!.*\\.\\.)(?=.{8,255})[A-Za-z0-9_%.+-]{2,64}@[A-Za-z0-9.-]{2,}\\.[A-Za-z]{2,}$";
 
     private final GenericApplicationContext context;
     private final UserService userService;
@@ -42,14 +43,21 @@ public class LoginService {
     }
 
     public void register(final String username, final String password, final String passwordCheck){
-        if (passwordCheck.equals(password)){
+        if (username.matches(REGEX_USERNAME_VALIDATOR) && passwordCheck.equals(password)){
             final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
             final String hashedPassword = passwordEncoder.encode(password);
             final User newUser = new User(username, hashedPassword);
             final User savedUser = userService.save(newUser);
         }
         else {
-            throw new LoginFailedException("Passwörter stimmen nicht überein");
+            final String message;
+            if (!passwordCheck.equals(password)){
+                message = "Passwörter stimmen nicht überein";
+            }
+            else {
+                message = "Username erfüllt nicht die Anforderungen";
+            }
+            throw new LoginFailedException(message);
         }
     }
 }
